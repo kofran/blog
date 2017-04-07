c0ec69da2be3823aea6c0aea547d2da7cb83e603
kofran <kofran.amd@gmail.com>
2017-04-07 11:21:32 -0300

    first commit, blog creation, model defined

---
+++ blog/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils import timezone
+
+# modelo de la tabla posts.
+class Post(models.Model):
+    author = models.ForeignKey('auth.User') # autor
+    title = models.CharField(max_length=200) # titulo
+    text = models.TextField() # texto del post
+    category = models.CharField(max_length=200, default='General') # categoria
+    created_date = models.DateTimeField(default=timezone.now) # fecha de creacion
+    published_date = models.DateTimeField(blank=True, null=True) # fecha de publicacion
+
+    # metodo publicar
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    # motodo para obtener titulo
+    def __str__(self):
+        return self.title
