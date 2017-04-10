f1cadecccf97db664c406d1634f34a55946da829
kofran <kofran.amd@gmail.com>
2017-04-10 14:40:53 -0300

    agregado formulario para crear post, retocado blog.css

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
 ]
