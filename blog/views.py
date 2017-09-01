4cd58b95acd84cf8d1a135640810810c1c5e3454
Franco <kofran.amd@gmail.com>
2017-09-01 13:20:31 -0300

    agregada paginacion en home

--- blog/views.py
+++ blog/views.py
@@ -7,11 +7,25 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import Post, Comment
 from .forms import PostForm, CommentForm, SignUpForm
 from .decorators import check_recaptcha
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 
+# def home(request):
+#     posts = (Post.objects.filter(published_date__lte=timezone.now()).
+#             order_by('published_date').reverse())
+#     return render(request, 'blog/home.html', {'posts': posts})
+
 def home(request):
-    posts = (Post.objects.filter(published_date__lte=timezone.now()).
+    postslist = (Post.objects.filter(published_date__lte=timezone.now()).
             order_by('published_date').reverse())
+    page = request.GET.get('page', 1)
+    paginator = Paginator(postslist, 10)
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/home.html', {'posts': posts})
 
 def post_detail(request, pk):
